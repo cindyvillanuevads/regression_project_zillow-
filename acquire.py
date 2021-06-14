4c2aee0aa1762a90d2e729e9687bff432538c429
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-06-14 09:20:00 -0500

    clean my functions

--- acquire.py
+++ acquire.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-from sklearn.model_selection import train_test_split
+
 # turn off pink warning boxes
 import warnings
 warnings.filterwarnings("ignore")
@@ -19,12 +19,12 @@ def get_connection(db_name):
     from env import host, username, password
     return f'mysql+pymysql://{username}:{password}@{host}/{db_name}'
 
-# ************************************ generic acquire function***************************************************************
+# ************************************ generic acquire function ***************************************************************
 
 
 def get_data_from_sql(db_name, query):
     """
-    This function takes in a string for the name of the database I want to connect to
+    This function takes in a string for the name of the database that I want to connect to
     and a query to obtain my data from the Codeup server and return a DataFrame.
     db_name : df name in a string type
     query: aalready created query that was named as query 
@@ -33,7 +33,7 @@ def get_data_from_sql(db_name, query):
     SELECT * 
     FROM table_name;
     '''
-    df = get_data_from_sql('telco_churn', query)
+    df = get_data_from_sql('zillow', query)
     """
     df = pd.read_sql(query, get_connection(db_name))
     return df
@@ -42,47 +42,7 @@ def get_data_from_sql(db_name, query):
 
 
 
-# ***************************************************************************************************
-#                                     ZILLOW DB
-# ***************************************************************************************************
 
-#acquire data for the first time
-def get_new_zillow():
-    '''
-    This function reads in the zillow data from the Codeup db
-    and returns a pandas DataFrame with columns :
-     bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips 
-    '''
-    sql_query = '''
-    SELECT parcelid, bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet,
-    taxvaluedollarcnt
-    FROM properties_2017
-    JOIN predictions_2017 as pred USING (parcelid)
-    WHERE pred.transactiondate >= '2017-05-01' AND pred.transactiondate <= '2017-08-31'
-    AND propertylandusetypeid > 259 AND propertylandusetypeid  < 266;
-    '''
-    return pd.read_sql(sql_query, get_connection('zillow'))
-
-#acquire data main function 
-def get_zillow():
-    '''
-    This function reads in telco_churn data from Codeup database, writes data to
-    a csv file if a local file does not exist, and returns a df.
-    '''
-    if os.path.isfile('zillow.csv'):
-        
-        # If csv file exists, read in data from csv file.
-        df = pd.read_csv('zillow.csv', index_col=0)
-        
-    else:
-        
-        # Read fresh data from db into a DataFrame.
-        df = get_new_zillow()
-        
-        # Write DataFrame to a csv file.
-        df.to_csv('zillow.csv')
-        
-    return df
 
 
 # ****************************************************************************************************************************************
@@ -91,8 +51,7 @@ def get_zillow():
 
 def miss_dup_values(df):
     '''
-    this function takes a dataframe as input and will output metrics for missing values and duplicated rows, 
-    and the percent of that column that has missing values and duplicated rows
+    this function takes a dataframe as input and returns metrics for missing values and duplicated rows.
     '''
         # Total missing values
     mis_val = df.isnull().sum()

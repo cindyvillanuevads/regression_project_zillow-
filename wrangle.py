561dfad459ec052caa5ef8f7d44fe47e608077ac
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-09-02 16:24:03 -0500

    add a function

--- wrangle.py
+++ wrangle.py
@@ -112,3 +112,31 @@ def wrangle_zillow():
 
 
 
+def miss_dup_values(df):
+    '''
+    this function takes a dataframe as input and will output metrics for missing values and duplicated rows, 
+    and the percent of that column that has missing values and duplicated rows
+    '''
+        # Total missing values
+    mis_val = df.isnull().sum()
+        # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+        #total of duplicated
+    dup = df.duplicated().sum()  
+        # Percentage of missing values
+    dup_percent = 100 * dup / len(df)
+        # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+    mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
+        # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+           "There are " + str(mis_val_table_ren_columns.shape[0]) +
+           " columns that have missing values.")
+    print( "  ")
+    print (f"** There are {dup} duplicate rows that represents {round(dup_percent, 2)}% of total Values**")
+        # Return the dataframe with missing information
+    return mis_val_table_ren_columns
